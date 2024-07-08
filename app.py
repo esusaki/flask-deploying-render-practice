c3f3a3245fdd91082049c0ff9ab7aae80c49a6d7
esusaki <susaki2002@gmail.com>
2024-07-08 19:40:20 +0900

    test of adding sqlite

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask
+import sqlite3
 
 application = Flask(__name__)
 
+conn = sqlite3.connect("test.db")
+cursor = conn.cursor()
+
+cursor.execute("select * from people;")
+
+people = cursor.fetchall()
+
 @application.route("/")
 def hello():
-    return ("<h1>hello render!</h1>")
+    return (f"<h1>Hello {people[0][0]} !</h1>")
 
 if __name__ == "__main__":
     application.run(debug=True)
